ce1deba236213c71aa26283ab733ce4c9d6cac27
Hyunsol Park <hpark0011@gmail.com>
2025-11-20 16:58:44 +0900

    add summary icon

--- app/analytics/_components/mindscore/widget/training-completed-status.tsx
+++ app/analytics/_components/mindscore/widget/training-completed-status.tsx
@@ -7,6 +7,7 @@ import {
   TooltipTrigger,
   TooltipContent,
 } from "@/components/ui/tooltip";
+import { ChevronRightIcon } from "lucide-react";
 
 export interface TrainingCompletedStatusProps {
   setShowCompletedStatus: (show: boolean) => void;
@@ -38,7 +39,8 @@ export function TrainingCompletedStatus({
         }
       }}
     >
-      <div className='flex items-center gap-0.5 relative py-0.5 px-2 w-full'>
+      <div className='flex items-center gap-0.5 relative py-0.5 px-2 w-full pl-1'>
+        <ChevronRightIcon className='size-3.5 text-icon-light' />
         <div className='text-[13px] font-[500] w-full'>Learning completed!</div>
       </div>
       <div className='text-xs hover:bg-extra-light dark:bg-black rounded-full text-text-muted flex items-center gap-1 mr-1'>
@@ -76,10 +78,19 @@ export function TrainingCompletedStatus({
           </Tooltip>
         )}
       </div>
-      <Icon
-        name='ArrowUpRightIcon'
-        className='size-4.5 text-text-muted group-hover:text-blue-400'
-      />
+      <Tooltip>
+        <TooltipTrigger asChild className='shadow-2xl'>
+          <div className='mr-1 flex items-center gap-0.5'>
+            <Icon
+              name='DocPlainTextFillIcon'
+              className='size-4 text-icon-light group-hover:text-blue-400'
+            />
+          </div>
+        </TooltipTrigger>
+        <TooltipContent className='shadow-[0_0_0_1px_rgba(255,255,255,0.05)]'>
+          View summary
+        </TooltipContent>
+      </Tooltip>
     </div>
   );
 }
