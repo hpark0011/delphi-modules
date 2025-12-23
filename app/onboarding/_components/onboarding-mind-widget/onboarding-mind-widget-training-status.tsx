eb095eed4142a06321142b5768e53b93fbf1fbd4
Hyunsol Park <hpark0011@gmail.com>
2025-12-24 06:56:11 +0900

    change the font size of the training status

--- app/onboarding/_components/onboarding-mind-widget/onboarding-mind-widget-training-status.tsx
+++ app/onboarding/_components/onboarding-mind-widget/onboarding-mind-widget-training-status.tsx
@@ -20,7 +20,7 @@ export function OnboardingMindWidgetTrainingStatus({
     >
       <div className='pl-2.5 pr-3.5 text-text-tertiary flex items-center gap-1 whitespace-nowrap'>
         <MindStatusIcon status='active' />
-        <span className='max-w-[176px] truncate text-[13px]'>
+        <span className='max-w-[176px] truncate text-[14px]'>
           {trainingMessage}
         </span>
       </div>
