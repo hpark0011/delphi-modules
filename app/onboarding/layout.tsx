3fcf915e9f1252ba29a050835edee337ab325f3e
Hyunsol Park <hpark0011@gmail.com>
2025-12-23 14:10:05 +0900

    import style component

--- app/onboarding/layout.tsx
+++ app/onboarding/layout.tsx
@@ -1,6 +1,7 @@
 import { MindDialog } from "@/components/mind-dialog/mind-dialog-2";
 import { OnboardingHeader } from "@/app/onboarding/_components/onboarding-header";
 import { OnboardingProviders } from "@/app/onboarding/_context";
+import "./_styles/mind-widget.styles.css";
 
 export default function OnboardingLayout({
   children,
